ff5b76640fb44c87a1b8f8a89e7f8162c191fb62
Suleiman <suleimanj225@gmail.com>
2025-07-10 15:06:46 +0300

    made corrections

--- Part2_Practical_Implementation/Task1_Code_Completion/manual_sort.py
+++ Part2_Practical_Implementation/Task1_Code_Completion/manual_sort.py
@@ -8,4 +8,5 @@ if __name__ == "__main__":
     data = [{"name": "Alice", "age": 25}, {"name": "Bob", "age": 20}]
     sorted_data = sort_dicts_by_key(data, "age")
     print(sorted_data)
+    
 # Output: [{'name': 'Bob', 'age': 20}, {'name': 'Alice', 'age': 25}]
