930878748fb5229bcd820bd536a95500d40ba3e9
Suleiman Juma <114980164+SuleimanJuma@users.noreply.github.com>
2025-07-10 13:32:00 +0300

    Update manual_sort.py

--- Part2_Practical_Implementation/Task1_Code_Completion/manual_sort.py
+++ Part2_Practical_Implementation/Task1_Code_Completion/manual_sort.py
@@ -1,7 +1,1 @@
-def sort_dicts_by_key(list_of_dicts, key):
-    return sorted(list_of_dicts, key=lambda x: x[key])
 
-if __name__ == "__main__":
-    data = [{"name": "Alice", "age": 25}, {"name": "Bob", "age": 20}]
-    sorted_data = sort_dicts_by_key(data, "age")
-    print(sorted_data)
